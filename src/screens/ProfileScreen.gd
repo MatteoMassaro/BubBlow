3f91a24c1423d3c6cd9ca4fae04d4929bd9e4c8c
Visci Nicolas <visciniko2000@gmail.com>
2023-05-11 10:49:31 +0200

    Salvataggio tipo_utente

--- src/screens/ProfileScreen.gd
+++ src/screens/ProfileScreen.gd
@@ -1,18 +1,15 @@
 extends Control
 
 onready var http : HTTPRequest = $HTTPRequest
-onready var tipo_utente : LineEdit = $Container/VBoxContainer2/Class/LineEdit
-onready var notification : Label = $Container/Notification
+onready var tipo_utente = ""
+onready var notification = ""
 
 var new_profile := false
 var information_sent := false
 var profile := {
 	"tipo_utente": {}
 } setget set_profile
-
-
-func _ready() -> void:
-	Firebase.get_document("users/%s" % Firebase.user_info.id, http)
+	
 
 func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
 	var result_body := JSON.parse(body.get_string_from_ascii()).result as Dictionary
@@ -23,30 +20,35 @@ func _on_HTTPRequest_request_completed(result: int, response_code: int, headers:
 			return
 		200:
 			if information_sent:
-				notification.text = "Information saved successfully"
+				#notification.text = "Information saved successfully"
 				information_sent = false
 			self.profile = result_body.fields
 
-func _on_ConfirmButton_pressed() -> void:
-	if tipo_utente.text.empty():
-		notification.text = "Please, enter your nickname and class"
-		return
-	
-	profile.tipo_utente = { "stringValue": tipo_utente.text }
-	
+
+func set_profile(value: Dictionary) -> void:
+	profile = value
+	tipo_utente = profile.tipo_utente.stringValue
+
+
+func _on_Userbutton_pressed():
+	profile.tipo_utente = { "stringValue": "user" }
 	match new_profile:
 		true:
 			Firebase.save_document("users?documentId=%s" % Firebase.user_info.id, profile, http)
 		false:
 			Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
 	information_sent = true
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_tree().change_scene("res://src/screens/LoginScreen.tscn")
 
-func set_profile(value: Dictionary) -> void:
-	profile = value
-	tipo_utente.text = profile.tipo_utente.stringValue
 
-func _on_Button_pressed():
-	get_tree().quit()
-
-func _on_BackButton_pressed():
-	get_tree().change_scene("res://src/screens/MenuScreen.tscn")
+func _on_MedicButton_pressed():
+	profile.tipo_utente = { "stringValue": "medic" }
+	match new_profile:
+		true:
+			Firebase.save_document("users?documentId=%s" % Firebase.user_info.id, profile, http)
+		false:
+			Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
+	information_sent = true
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_tree().change_scene("res://src/screens/LoginScreen.tscn")
--- static/Firebase.gd
+++ static/Firebase.gd
@@ -1,16 +1,28 @@
 extends Node
 
 const API_KEY := "AIzaSyBVxyykoeGfD71gD1YFWkErgOaj9tS4XnE"
+const PROJECT_ID := "bubblow-4b8ea"
 
 const REGISTER_URL := "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=%s" % API_KEY
 const LOGIN_URL := "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=%s" % API_KEY
+const FIRESTORE_URL := "https://firestore.googleapis.com/v1/projects/%s/databases/(default)/documents/" % PROJECT_ID
 
-var current_token := ""
+var user_info := {}
 
 
-func _get_token_id_from_result(result: Array) -> String:
+func _get_user_info(result: Array) -> Dictionary:
 	var result_body := JSON.parse(result[3].get_string_from_ascii()).result as Dictionary
-	return result_body.idToken
+	return {
+		"token": result_body.idToken,
+		"id": result_body.localId
+	}
+
+
+func _get_request_headers() -> PoolStringArray:
+	return PoolStringArray([
+		"Content-Type: application/json",
+		"Authorization: Bearer %s" % user_info.token
+	])
 
 
 func register(email: String, password: String, http: HTTPRequest) -> void:
@@ -21,15 +33,40 @@ func register(email: String, password: String, http: HTTPRequest) -> void:
 	http.request(REGISTER_URL, [], false, HTTPClient.METHOD_POST, to_json(body))
 	var result := yield(http, "request_completed") as Array
 	if result[1] == 200:
-		current_token = _get_token_id_from_result(result)
+		user_info = _get_user_info(result)
 
 
 func login(email: String, password: String, http: HTTPRequest) -> void:
 	var body := {
 		"email": email,
 		"password": password,
+		"returnSecureToken": true
 	}
 	http.request(LOGIN_URL, [], false, HTTPClient.METHOD_POST, to_json(body))
 	var result := yield(http, "request_completed") as Array
 	if result[1] == 200:
-		current_token = _get_token_id_from_result(result)
+		user_info = _get_user_info(result)
+
+
+func save_document(path: String, fields: Dictionary, http: HTTPRequest) -> void:
+	var document := { "fields": fields }
+	var body := to_json(document)
+	var url := FIRESTORE_URL + path
+	http.request(url, _get_request_headers(), false, HTTPClient.METHOD_POST, body)
+
+
+func get_document(path: String, http: HTTPRequest) -> void:
+	var url := FIRESTORE_URL + path
+	http.request(url, _get_request_headers(), false, HTTPClient.METHOD_GET)
+
+
+func update_document(path: String, fields: Dictionary, http: HTTPRequest) -> void:
+	var document := { "fields": fields }
+	var body := to_json(document)
+	var url := FIRESTORE_URL + path
+	http.request(url, _get_request_headers(), false, HTTPClient.METHOD_PATCH, body)
+
+
+func delete_document(path: String, http: HTTPRequest) -> void:
+	var url := FIRESTORE_URL + path
+	http.request(url, _get_request_headers(), false, HTTPClient.METHOD_DELETE)
