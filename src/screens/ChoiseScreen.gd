3853e2d79be0298e6e57f5a6587bacb453f5268e
Visci Nicolas <visciniko2000@gmail.com>
2023-05-22 16:28:25 +0200

    Refactoring e caricamento mail

--- src/screens/ChoiseScreen.gd
+++ src/screens/ChoiseScreen.gd
@@ -2,14 +2,15 @@ extends Control
 
 onready var http : HTTPRequest = $HTTPRequest
 onready var email = ""
-onready var notification : Label = $Container/Notification
-onready var user_button : Button = $Menu/Userbutton
-onready var medic_button : Button = $Menu/MedicButton
+onready var notification_panel : PanelContainer = $NotificationPanel
+onready var notification : Label = $NotificationPanel/Notification
+onready var patient_button : Button = $Menu/PatientButton
+onready var doctor_button : Button = $Menu/DoctorButton
 
 var information_sent := false
 var profile := {
 	"email": {},
-	"tipo_utente": {}
+	"type_user": {}
 } 
 
 func _ready():
@@ -21,38 +22,43 @@ func _on_HTTPRequest_request_completed(result: int, response_code: int, headers:
 	match response_code:
 		404:
 			notification.text = "Please, enter your information"
+			show_label()
 			return
 		200:
 			if information_sent:
-				#notification.text = "Information saved successfully"
+				notification.text = "Registrazione avvenuta con successo"
+				show_label()
 				information_sent = false
 
 
 func _on_Userbutton_pressed():
-	profile.tipo_utente = { "stringValue": "patient" }
+	profile.type_user = { "stringValue": "patient" }
 	profile.email = { "stringValue": email}
 	Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
 	information_sent = true
-	user_button.rect_scale = Vector2(0.8, 0.8)
+	patient_button.rect_scale = Vector2(0.8, 0.8)
 	if AudioManager.flag_effects == 0:
 		AudioManager.effect_track = load("res://assets/user interface/sounds/kenney_interfacesounds/Audio/drop_004.ogg")
 		AudioManager.play_effect()
 	yield(get_tree().create_timer(0.1), "timeout")
-	user_button.rect_scale = Vector2(1, 1)
+	patient_button.rect_scale = Vector2(1, 1)
 	yield(get_tree().create_timer(2.0), "timeout")
-	get_tree().change_scene("res://src/screens/LoginScreen.tscn")
+	get_tree().change_scene("res://src/screens/MenuScreen.tscn")
 
 
 func _on_MedicButton_pressed():
-	profile.tipo_utente = { "stringValue": "doctor" }
+	profile.type_user = { "stringValue": "doctor" }
 	profile.email = { "stringValue": email}
 	Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
 	information_sent = true
-	medic_button.rect_scale = Vector2(0.8, 0.8)
+	doctor_button.rect_scale = Vector2(0.8, 0.8)
 	if AudioManager.flag_effects == 0:
 		AudioManager.effect_track = load("res://assets/user interface/sounds/kenney_interfacesounds/Audio/drop_004.ogg")
 		AudioManager.play_effect()
 	yield(get_tree().create_timer(0.1), "timeout")
-	medic_button.rect_scale = Vector2(1, 1)
+	doctor_button.rect_scale = Vector2(1, 1)
 	yield(get_tree().create_timer(2.0), "timeout")
-	get_tree().change_scene("res://src/screens/LoginScreen.tscn")
+	get_tree().change_scene("res://src/screens/MenuScreen.tscn")
+
+func show_label():
+	notification_panel.show()
