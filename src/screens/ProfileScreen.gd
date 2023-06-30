fae51c4df67617bfc9db239755fe3e4725d77b53
matteo massaro <massaromatteo21@gmail.com>
2023-06-30 14:42:09 +0200

    fix label profilo

--- src/screens/ProfileScreen.gd
+++ src/screens/ProfileScreen.gd
@@ -12,7 +12,7 @@ func set_profile():
 	item_list.set_item_text(0, "Nome: " + PlayerData.name_user)
 	item_list.set_item_text(1, "Cognome: " + PlayerData.surname_user)
 	item_list.set_item_text(2, "Email: " + PlayerData.email)
-	item_list.set_item_text(3, "Highscore modalita' classica: " + str(PlayerData.highscore_first_mode))
-	item_list.set_item_text(4, "Highscore modalita' runner: " + str(PlayerData.highscore_second_mode))
-	item_list.set_item_text(5, "Highscore modalita' bersagli: " + str(PlayerData.highscore_third_mode))
+	item_list.set_item_text(3, "Highscore livello bolle: " + str(PlayerData.highscore_first_mode))
+	item_list.set_item_text(4, "Highscore livello runner: " + str(PlayerData.highscore_second_mode))
+	item_list.set_item_text(5, "Highscore livello bersagli: " + str(PlayerData.highscore_third_mode))
 
